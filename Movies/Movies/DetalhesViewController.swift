facaee068c6f25416291ddcb49f08c6dcf1cbe7d
nikolasgianoglou <nikolasgcoelho@gmail.com>
2022-02-14 11:46:09 -0300

    Criacao do layout basico das telas, adicao da navigation controller

---
+++ Movies/Movies/DetalhesViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetalhesViewController.swift
+//  Movies
+//
+//  Created by Nikolas Gianoglou Coelho on 14/02/22.
+//
+
+import UIKit
+
+class DetalhesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+ 
+
+}
