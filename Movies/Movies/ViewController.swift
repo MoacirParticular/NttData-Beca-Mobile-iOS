8f1dc69ccfc94918b761a6924e900789e21affed
nikolasgianoglou <nikolasgcoelho@gmail.com>
2022-02-14 10:50:33 -0300

    Criacao do projeto

---
+++ Movies/Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Nikolas Gianoglou Coelho on 14/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
