03d55dd1d673f1668f03acc95b28af2ab5ea5f0f
Rodiney Branta <branta.rodiney@gmail.com>
2022-02-15 11:25:51 -0300

    Cinespetaculo Project

---
+++ cinespetaculo/cinespetaculo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cinespetaculo
+//
+//  Created by Rodiney Branta on 14/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
